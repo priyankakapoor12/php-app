21d24470bf391fe51c8e60d689ec55f95a1d4aca
priyankakapoor12 <priyankakapoor1993@gmail.com>
2026-04-14 19:05:03 +0000

    new file

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
-$conn = new mysqli("my-db.cozka4i8kv1n.us-east-1.rds.amazonaws.com", "admin", "priyanka", "mydb");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$result = $conn->query("SELECT * FROM users");
-
-while($row = $result->fetch_assoc()) {
-    echo $row['name'] . "<br>";
-}
+echo "Hello Priyanka - Version 2";
 ?>
